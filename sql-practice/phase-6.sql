ae7a92df488bd73e0433483b8a653a8f44545ed2
Haris Ahmed <iamharis08@gmail.com>
2022-10-06 19:27:39 -0400

    last

--- sql-practice/phase-6.sql
+++ sql-practice/phase-6.sql
@@ -1,2 +1,15 @@
 -- Update the toy with the name of "Cheetos" to have a name of "Pooky"
 -- Your code here
+SELECT toys.name FROM cat_owners
+JOIN owners ON (cat_owners.owner_id = owners.id)
+JOIN toys ON (cat_owners.cat_id = toys.cat_id)
+-- WHERE toys.name = 'Cheetos';
+
+-- UPDATE toys
+-- SET name = 'Pooky'
+-- WHERE name = 'Cheetos';
+-- SELECT name FROM toys
+UPDATE cat_owners
+SET cat_id = 'Pooky'
+WHERE cat_id = 'Cheetos';
+SELECT name FROM toys;
